9ebdaa4e37d7f83bbd2415e3f2bb560abfbde78f
leelohoon <21600558@handong.edu>
2019-05-24 17:01:16 +0900

    save

--- ChatClient.java
+++ ChatClient.java
@@ -1,8 +1,5 @@
 import java.net.*;
 import java.io.*;
-//아래의 class 구현 중 필요한 class를 가져와서 쓰기 위해 import함
-//java.net package 안에 있는 class: Socket
-//java.io package 안에 있는 class: BufferedReader, PrintWriter, OutoutStreamWriter, InputStremReader,
 
 public class ChatClient {
 
@@ -10,32 +7,24 @@ public class ChatClient {
 		if(args.length != 2){
 			System.out.println("Usage : java ChatClient <username> <server-ip>");
 			System.exit(1);
-		}//초기 입력값이 3개의 파라미터가 아니면 메세지 던지고 종료
+		}
 		Socket sock = null;
 		BufferedReader br = null;
 		PrintWriter pw = null;
 		boolean endflag = false;
 		try{
 			sock = new Socket(args[1], 10001);
-// 본인 ip와 server의 포트를 연결
 			pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
-//컨셉:sock안에서 아웃풋 스트림을 리턴하고 그걸 받은 아웃풋 스트림 라이터 객체를 만들고 그걸 받은 프린트 라이터 객체를 만들고 그것을 이용해 인스턴스를 만듦
-			
-			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-//컨셉:sock안에서 인풋 스트림을 리턴하고 그걸 받은 인풋 스트림 리더라는 객체를 만들고 그것을 파라미터로 받은 버퍼리터 객체를 만들고 그것을 이용해 인스턴스를 만듦 
+			br = new BufferedReader(new InputStreamReader(sock.getInputStream())); 
+				
+			BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 		
 			
 			
-			BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-			//BufferedReader는 문장을 바로 읽을 수 없음 여러 컨버터를 연결하는 것 처럼InputStreamReader가 젠더 역할 수행하여 client가 입력한 내용을 이용
-			
-			
 			pw.println(args[0]);
-			//유저 name을 파라미터로 system.out.println 과 같은 기능이지만 종착역이 다름.
-			//pw에 입력하는 기능 
 			pw.flush();
 			InputThread it = new InputThread(sock, br);
-			it.start(); //ChatServer와 마찬가지로 InputThread의 run을 실행
+			it.start(); 
 			String line = null;
 			while((line = keyboard.readLine()) != null){
 				pw.println(line);
@@ -44,11 +33,11 @@ public class ChatClient {
 					endflag = true;
 					break;
 				}
-			}//while loop를 계속 돌면서 /quit 이 나오기 전까지 계속 입력이 들어오기를 기다림 이때 입력이 들어오는 순간 ChatServer는 들어온 입력값을 받아 처리
-			System.out.println("Connection closed.");//  	/quit을 받은 경우 실행
+			}
+			System.out.println("Connection closed.");
 		}catch(Exception ex){
 			if(!endflag)
-				System.out.println(ex);  // /quit이 원인이 아닌 다른 ex라는 error 발생시 
+				System.out.println(ex);  
 		}finally{
 			try{
 				if(pw != null)
@@ -62,7 +51,7 @@ public class ChatClient {
 				if(sock != null)
 					sock.close();
 			}catch(Exception ex){}
-		} // connection 을 끊을때 pw,br,sock 이 null이 아닌경우는 닫아 줘야함.
+		} 
 	} 
 } 
 
@@ -72,12 +61,12 @@ class InputThread extends Thread{
 	public InputThread(Socket sock, BufferedReader br){
 		this.sock = sock;
 		this.br = br;
-	} //constructor 이며 개인에게 할당된 socket과 입력되어 있는 문장들을 읽어들일 BufferedReader를 파라미터로 받
+	} 
 	public void run(){
 		try{
 			String line = null;
 			while((line = br.readLine()) != null){
-				System.out.println(line); //입력된 문장들을 해당  client의 화면에 띄우기 위한 과정, 위에서 close하면 br.readline()이 null됨 
+				System.out.println(line); 
 			}
 		}catch(Exception ex){
 		}finally{
--- ChatServer.java
+++ ChatServer.java
@@ -1,31 +1,25 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
-//아래의 class 구현 중 필요한 class를 가져와서 쓰기 위해 import함
-//java.net package 안에 있는 class: Serversocket method, Socket method,
-//java.util package 안에 있는 class: Hashmap, Collection, Iterator
-//java.io package 안에 있는 class: BufferedReader, PrintWriter, OutoutStreamWriter, InputStremReader, 
 
 
-public class ChatServer { //chatserver 의 instance는 언제 만들어지나? 안만들어짐.
+public class ChatServer { 
 
-	public static void main(String[] args) { // start 지점(class method/static method) - 객체없어도 실행가능
+	public static void main(String[] args) {  
 
 try{
-			ServerSocket server = new ServerSocket(10001);//사용할 ip주소
+			ServerSocket server = new ServerSocket(10001);
 			System.out.println("Waiting connection...");
 			HashMap hm = new HashMap();
 			while(true){
 				Socket sock = server.accept();
 				ChatThread chatthread = new ChatThread(sock, hm);
-//chatthread는 ChatThread의 instance 이며 ChatThread의 constructor로 생성됨(sock과 hm을 parameter로 받음)
 				chatthread.start();
-//ChatThread class 는 Thread class 를 상속받는데 Thread class 안에 있는 start method를 통해 ChatThread에 override 되어있는 run method를 실행한다.
-			} // while
+			} 
 		}catch(Exception e){
 			System.out.println(e);
-		}//try중에 error가 발생하는 경우 Exception 이라는 class의 instance를 만든 후 에러 내용 print해줌
-	} // main
+		}
+	} 
 }
 
 class ChatThread extends Thread{
@@ -36,24 +30,22 @@ class ChatThread extends Thread{
 	private boolean initFlag = false;
 	public ChatThread(Socket sock, HashMap hm){
 		this.sock = sock;
-		this.hm = hm; //main에 있는 hm의 reference를 가져옴 (실체 x)
+		this.hm = hm; 
 		try{
-			PrintWriter pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));//출력하는 역할
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
 			
-			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));//read line하는 역할
+			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 			id = br.readLine();
-			broadcast(id + " entered."); //개개인에 배당된 chattread가 모든 client들에게 전달
+			broadcast(id + " entered."); 
 			System.out.println("[Server] User (" + id + ") entered.");
 			synchronized(hm){
 				hm.put(this.id, pw);
-			}//동기화 기능 수행 hm이라는 instance에 들어오는 id와 printwriter를 hm에 저장 및 동기화(이 과정이 없으면 충돌 발생--> 줄세우기)
-			//thread를 사용할때 가장 신경써야 하는 부분이 충돌
-
+			}
 			initFlag = true;
 		}catch(Exception ex){
 			System.out.println(ex);
-		} //ex라는 Exception instance 를 에러가 발생할 시 만들어 print
-	} // 여기 까지가 ChatThread의 construcor
+		} 
+	} 
 	public void run(){
 		boolean check;
 		try{
@@ -61,13 +53,11 @@ class ChatThread extends Thread{
 			while((line = br.readLine()) != null){ 
 				if(line.equals("/quit"))
 					break;
-//client로 부터 받아온 line이 /quit 이라면 break하고 while문 나옴
 				if(line.indexOf("/to ") == 0){
 					check=checkline(line);
 					if(check==false)
 						sendmsg(line);
 					else warning();
-//client로 부터 받아온 line이 /to 라면 sendmsg method 실행
 				}
 				else if(line.equals("/userlist")) {
 					send_userlist();
@@ -77,58 +67,43 @@ class ChatThread extends Thread{
 					if(check==false)
 						broadcast(id + " : " + line);
 					else warning();}
-//그 외의 경우엔 모두 broadcast 실행
 			}//
 		}catch(Exception ex){ 
 			System.out.println(ex);  
-//ex라는 Exception instance 를 에러가 발생할 시 만들어 print
 		}finally{
-//finally는 위에 에러가 발생하든 안하든 실행
 			synchronized(hm){
 				hm.remove(id);
-			}//hm에서 해당 id를 삭제하고 동기화
-			broadcast(id + " exited."); //나갔다고 전함
+			}
+			broadcast(id + " exited."); 
 			try{ 
 				if(sock != null)
-					sock.close();  //sock이 null이 아니라면 그 socket을 close함(연결끊음)
+					sock.close();  
 			}catch(Exception ex){} 
-//ex라는 Exception instance 를 에러가 발생할 시 만듦
 
 		}
-	} // Thread에도 run method가 있지만 여기서 override로 같은 method를 정의
+	} 
 	public void sendmsg(String msg){
 		int start = msg.indexOf(" ") +1; 
-//이 method는 /to를 포함하는 string을 파라미터로 받기 때문에 첫 blanck space가 나온 다음 index부터 메세지를 보내고자하는 client명의 시작이 된다. (ex. /to park hi)
 		int end = msg.indexOf(" ", start);
-//다음 blanck space가 나오는 곳이 end index 
-		if(end != -1){ //end index가 있는경우
+		if(end != -1){ 
 			String to = msg.substring(start, end); 
-			//start ~ end index 앞까지가 받을 client 명
 			String msg2 = msg.substring(end+1);
-			//두번째 blanck space 이후 부터가 보낼 message
-			Object obj = hm.get(to); //읽든 쓰든 sync를 쓰는게 안전.
-
-	      //hm안에 해당 id(client)가 있는지 확인하고 있다면 printwriter return 
+			Object obj = hm.get(to); 
 			if(obj != null){
 				PrintWriter pw = (PrintWriter)obj; 
-//printwriter class는 object class를 상속하므로 이런 선언 가능
 				pw.println(id + " whisphered. : " + msg2);
-//printwriter 화면에 print함
 				pw.flush();
-//물내리기(해당 printwriter안에 println청소
-			} // if
+			} 
 		}
-	} // sendmsg
+	}
 	public void broadcast(String msg){
 		try {
-		//PrintWriter myPw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
 			synchronized(hm){
 				Object obj = hm.get(id);
 				PrintWriter myPw = (PrintWriter) obj;
 				Collection collection = hm.values();
 				Iterator iter = collection.iterator(); 
-//hm에 있던 values들 즉, 각 socket의 printwriter들을 모아 저장
-				while(iter.hasNext()){ //다음이 있으면 true 없으면 false
+				while(iter.hasNext()){
 					PrintWriter pw = (PrintWriter)iter.next();
 					if(!pw.equals(myPw)) {
 						pw.println(msg);
@@ -136,13 +111,12 @@ class ChatThread extends Thread{
 					}
 
 				} 
-//pw를 바꿔가면서 println 실행(존재하는 모든 pw에 println실행)
+
 		}
 	}catch(Exception e){
 		System.out.println(e);
-	} // broadcast
+	} 
 	}
-	//함수 호출한 client의 pw 로 전체 hm의 keySet을 print pw를어떻게?
 	public void send_userlist(){
 			synchronized(hm) {
 				Object obj = hm.get(id);
